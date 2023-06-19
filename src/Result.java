13acde2f1e8928bfcf8c95b50019c859b3bd31b9
IlyaYakubski <yakubski.ilya@student.ehu.lt>
2023-06-20 00:23:48 +0300

    a

--- src/Result.java
+++ src/Result.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Result {
+import java.sql.Date;
+
+public class Result {
+    private String login;
+    private String test;
+    private java.sql.Date date;
+    private int mark;
+
+    public Result(String login, String test, Date date, int mark) {
+        this.login = login;
+        this.test = test;
+        this.date = date;
+        this.mark = mark;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class Runner {
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        JSONParser parser = new JSONParser();
+        List<Result> results = new ArrayList<>();
+
+        try {
+            Object obj = parser.parse(new FileReader("src/results.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONArray resultsArray = (JSONArray) jsonObject.get("results");
+
+            for (Object resultObj : resultsArray) {
+                JSONObject resultJson = (JSONObject) resultObj;
+
+                for (Object testObj : resultJson.values()) {
+                    JSONArray testArray = (JSONArray) testObj;
+
+                    for (Object entryObj : testArray) {
+                        JSONObject entryJson = (JSONObject) entryObj;
+                        String login = resultJson.keySet().iterator().next().toString();
+                        String test = entryJson.keySet().iterator().next().toString();
+                        String dateString = ((JSONObject) entryJson.get(test)).get("date").toString();
+                        Date date = Date.valueOf(dateString);
+                        double markDouble = (Double) ((JSONObject) entryJson.get(test)).get("mark");
+                        int mark = (int) markDouble;
+                        results.add(new Result(login, test, date, mark));
+                    }
+                }
+            }
+
+            for (Result result : results) {
+                System.out.println(result.getLogin() + ";" +
+                        result.getTest() + ";" +
+                        result.getDate() + ";" +
+                        result.getMark());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
